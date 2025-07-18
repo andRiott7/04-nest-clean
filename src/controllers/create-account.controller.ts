36e0fbacfd72c84c327ed78fac1fb3ec593b3c2e
andRiott7 <andriott7@gmail.com>
2025-07-18 11:13:43 -0300

    feat: - Pipe validation with zod, - Rest client file and lib extension

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,15 +1,16 @@
-import { Body, ConflictException, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, ConflictException, Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { hash } from 'bcryptjs';
 import { z } from 'zod';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 
-const CreateAccountBodySchema = z.object({
+const createAccountBodySchema = z.object({
   name: z.string(),
   email: z.string().email(),
   password: z.string(),
 })
 
-type CreateAccountBodySchema = z.infer<typeof CreateAccountBodySchema>
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -18,9 +19,10 @@ export class CreateAccountController {
 
   @Post()
   @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
   async handle(@Body() body: CreateAccountBodySchema) {
 
-    const { name, email, password } = CreateAccountBodySchema.parse(body)
+    const { name, email, password } = createAccountBodySchema.parse(body)
 
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
