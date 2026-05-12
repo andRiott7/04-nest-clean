bc05b3b154332fe4cc10a9365a5f86140bd25701
andRiott7 <andriott7@gmail.com>
2026-05-12 11:58:21 -0300

    feat: - controller for listing questions with pagination

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthenticateController } from './controllers/authenticate.controller';
 import { CreateQuestionController } from './controllers/create-question.controller';
 import { envSchema } from './env';
 import { AuthModule } from './auth/auth.module';
+import { FetchRecentQuestionController } from './controllers/fetch-recent-questions.controller';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -14,7 +15,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     AuthModule,
   ],
-  controllers: [CreateAccountController, AuthenticateController, CreateQuestionController],
+  controllers: [CreateAccountController, AuthenticateController, CreateQuestionController, FetchRecentQuestionController],
   providers: [PrismaService],
 })
 export class AppModule { }
