c56bb0f1fabd1304149708cd7d471cccda503464
andRiott7 <andriott7@gmail.com>
2026-05-13 21:58:37 -0300

    feat: - including domain and core folders from clean project

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// @ts-check
+const eslint = require('@eslint/js')
+const tseslint = require('typescript-eslint')
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+)
