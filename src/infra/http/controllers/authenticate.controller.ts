39c4dff2f2f303e964de5237c8972b5a4b2e27a9
andRiott7 <andriott7@gmail.com>
2026-05-15 09:43:52 -0300

    refactor: - changing folders structure with new model

--- src/infra/http/controllers/authenticate.controller.ts
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -1,8 +1,8 @@
 import { Body, Controller, Post, UnauthorizedException, UsePipes } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
-import { ZodValidationPipe } from "@/pipes/zod-validation-pipe";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
 import { compare } from 'bcryptjs'
-import { PrismaService } from "@/prisma/prisma.service";
+import { PrismaService } from "@/infra/prisma/prisma.service";
 import { z } from 'zod';
 
 const authenticateBodySchema = z.object({
