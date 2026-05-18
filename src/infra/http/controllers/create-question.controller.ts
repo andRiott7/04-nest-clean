1847ec2906a4adf0898c6023fd6eb5847e84e1af
andRiott7 <andriott7@gmail.com>
2026-05-18 14:38:58 -0300

    refactor: - communication between layers

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -5,6 +5,7 @@ import { UserPayload } from '@/infra/auth/jwt.strategy'
 import z from "zod";
 import { PrismaService } from "@/infra/database/prisma/prisma.service";
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
@@ -18,7 +19,7 @@ type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   constructor(
-    private prisma: PrismaService
+    private createQuestion: CreateQuestionUseCase
   ) { }
 
   @Post()
@@ -29,26 +30,16 @@ export class CreateQuestionController {
     const { title, content } = body
     const userId = user.sub
 
-    const slug = this.convertToSlug(title)
 
-    await this.prisma.question.create({
-      data: {
-        authorId: userId,
-        title,
-        content,
-        slug,
-      }
+    await this.createQuestion.execute({
+      title,
+      content,
+      authorId: userId,
+      attachmentsIds: [],
     })
     console.log(user.sub)
     return 'ok'
   }
 
-  private convertToSlug(title: string) {
-    return title
-      .toLowerCase()
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-      .replace(/[^\w\s-]/g, '')
-      .replace(/\s+/g, '-')
-  }
+
 }
