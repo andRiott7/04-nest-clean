424f6aa2330584849289422b18bd789588e4b8bb
andRiott7 <andriott7@gmail.com>
2026-05-18 21:23:51 -0300

    feat:  - handling fetch recent questions

--- src/domain/forum/application/use-cases/fetch-recent-questions.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -1,6 +1,7 @@
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
 import { Either, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
 
 interface FetchRecentQuestionsUseCaseRequest {
   page: number
@@ -12,9 +13,9 @@ type FetchRecentQuestionsUseCaseResponse = Either<
     questions: Question[]
   }
 >
-
+@Injectable()
 export class FetchRecentQuestionsUseCase {
-  constructor(private questionsRepository: QuestionsRepository) {}
+  constructor(private questionsRepository: QuestionsRepository) { }
 
   async execute({
     page,
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { CreateAccountController } from '@/infra/http/controllers/create-account.controller';
 import { AuthenticateController } from '@/infra/http//controllers/authenticate.controller';
 import { CreateQuestionController } from '@/infra/http//controllers/create-question.controller';
-import { FetchRecentQuestionController } from '@/infra/http//controllers/fetch-recent-questions.controller';
+import { FetchRecentQuestionsController } from '@/infra/http//controllers/fetch-recent-questions.controller';
 import { DatabaseModule } from '@/infra/database/database.module';
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question';
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
 
 @Module({
   imports: [DatabaseModule],
@@ -12,8 +13,8 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
-    FetchRecentQuestionController
+    FetchRecentQuestionsController
   ],
-  providers: [CreateQuestionUseCase],
+  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
 })
 export class HttpModule { }
