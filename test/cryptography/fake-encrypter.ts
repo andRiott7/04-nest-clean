598e706c1617b180b10f48b30b39708834a8c998
andRiott7 <andriott7@gmail.com>
2026-05-19 21:53:15 -0300

    test: - for register and auth students

--- test/cryptography/fake-encrypter.ts
+++ test/cryptography/fake-encrypter.ts
@@ -2,6 +2,6 @@ import { Encrypter } from "@/domain/forum/application/cryptography/encrypter";
 
 export class FakeEncrypter implements Encrypter {
   async encrypt(payload: Record<string, unknown>): Promise<string> {
-    throw JSON.stringify(payload)
+    return JSON.stringify(payload)
   }
 }
